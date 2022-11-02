a7265d751ca759ff1567e6b793c5cf6d69db63de
Ana InAas Oliveira de Barros <up201806593@fe.up.pt>
2022-11-02 18:31:42 +0000

    Added: apted working example

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/astDiff.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/astDiff.java
@@ -1,5 +1,13 @@
 package edu.mit.csail.sdg.alloy4whole;
 
+
+import java.util.List;
+
+import at.unisalzburg.dbresearch.apted.costmodel.PerEditOperationStringNodeDataCostModel;
+import at.unisalzburg.dbresearch.apted.distance.APTED;
+import at.unisalzburg.dbresearch.apted.node.Node;
+import at.unisalzburg.dbresearch.apted.node.StringNodeData;
+import at.unisalzburg.dbresearch.apted.parser.BracketStringInputParser;
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.ErrorWarning;
 import edu.mit.csail.sdg.ast.Module;
@@ -8,6 +16,10 @@ import edu.mit.csail.sdg.parser.CompUtil;
 public class astDiff {
 
     public static void main(String[] args) {
+        apted();
+    }
+
+    public static void alloyParse(String[] args) {
         A4Reporter rep = new A4Reporter() {
 
             // For example, here we choose to display each "warning" by printing
@@ -25,6 +37,27 @@ public class astDiff {
             Module world = CompUtil.parseEverything_fromFile(rep, null, filename);
             world.showAsTree(null);
         }
+    }
+
+    public static void apted() {
+
+        // Parse the input and transform to Node objects storing node information in MyNodeData.
+        BracketStringInputParser parser = new BracketStringInputParser();
+        Node<StringNodeData> t1 = parser.fromString("{A{B{X}{Y}{F}}{C}}");
+        Node<StringNodeData> t2 = parser.fromString("{A{B{X}{Y}{F}}}");
+        // Initialise APTED.
+        APTED<PerEditOperationStringNodeDataCostModel,StringNodeData> apted = new APTED<>(new PerEditOperationStringNodeDataCostModel(1, 10, 0));
+        // Execute APTED.
+        float result = apted.computeEditDistance(t1, t2);
+        System.out.println("Edit distance = " + result);
+        List<int[]> mapping = apted.computeEditMapping();
+
+        for (int[] indexes : mapping)
+            System.out.println(indexes[0] + " -- " + indexes[1]);
+
+        System.out.println();
+        System.out.println(t1.getChildren());
+        System.out.println(t2.getChildren());
     }
 
 }
